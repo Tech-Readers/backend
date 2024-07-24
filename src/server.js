1615c07f6fa589d9eb2aa56a9b889121c5811ba1
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-07-24 06:13:37 -0300

    ok

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send(`Welcome to my server!`)
 })
 
-app.listen(port, () =>{
-    console.log(`Server running on the port ${port}`)
+app.listen(PORT, () =>{
+    console.log(`Server running on the port ${PORT}`)
 })
