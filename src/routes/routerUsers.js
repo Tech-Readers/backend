e7cfa64e05d348dca27a5c6d4cd03c5cd19cef9b
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-03 12:34:17 -0300

    OK

--- src/routes/routerUsers.js
+++ src/routes/routerUsers.js
@@ -5,15 +5,22 @@ import authMiddleware from '../middleware/authMiddleware.js';
 // cria uma instância do Router do Express para definir as rotas relacionadas aos usuários.
 const routerUsers = express.Router();
 
+
+// 1. ROTAS PUBLICAS:
 // GET / --> chama o método allUsers do userController para obter todos os usuários
 routerUsers.get('/', userController.allUsers);
 
+// POST / --> chama o método createUser do userController para criar um novo usuário
+routerUsers.post('/', userController.createUser);
+
+// POST /login -->  chama o método login do userController para autenticar um usuário e gerar um token JWT
+routerUsers.post('/login', userController.login);
+
+
+// 2. ROTAS PRIVADAS - AUTENTICADAS:
 // GET /:id -->  chama o método byIdUser do userController para obter um usuário específico pelo ID
 // usa authMiddleware para garantir que apenas usuários autenticados possam acessar essa rota
 routerUsers.get('/:id', authMiddleware, userController.byIdUser);
-
-// POST / --> chama o método createUser do userController para criar um novo usuário
-routerUsers.post('/', userController.createUser);
 
 // PUT /:id -->  chama o método updateUser do userController para atualizar um usuário específico pelo ID
 // usa authMiddleware para garantir que apenas usuários autenticados possam acessar essa rota
@@ -22,9 +29,6 @@ routerUsers.put('/:id', authMiddleware, userController.updateUser);
 // DELETE /:id -->  chama o método deleteUser do userController para deletar um usuário específico pelo ID
 // usa authMiddleware para garantir que apenas usuários autenticados possam acessar essa rota
 routerUsers.delete('/:id', authMiddleware, userController.deleteUser);
-
-// POST /login -->  chama o método login do userController para autenticar um usuário e gerar um token JWT
-routerUsers.post('/login', userController.login);
 
 // GET /profile -->  Chama o método userProfile do userController para obter os dados do perfil do usuário autenticado
 // usa authMiddleware para garantir que apenas usuários autenticados possam acessar essa rota
