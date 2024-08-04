2225fed97ba097c52e54e8bc2a9510ac00191afb
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-04 11:34:23 -0300

    correcao_exchange

--- src/routes/routerExchange.js
+++ src/routes/routerExchange.js
@@ -1,35 +1,36 @@
 // routerExchange.js:
 import express from 'express';
 import exchangeController from '../controllers/exchangeController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 
 //Cria uma instância do router do express para definir as rotas relacionadas aos anuncios
 const routerExchanges = express.Router();
 
 // GET / --> Chama o método allExchanges do exchangeController para obter todos os anuncios
-routerExchanges.get('/', exchangeController.allExchanges); 
+routerExchanges.get('/', authMiddleware, exchangeController.allExchanges); 
 
 // GET /:id --> Chama o método exchangeById do exchangeController para obter um anuncio específico pelo ID
-routerExchanges.get('/:id', exchangeController.exchangeById);
+routerExchanges.get('/:id', authMiddleware, exchangeController.exchangeById);
 
 // GET /:userId --> Chama o método exchangesByUserId para obter todos os anúncios de um usuário específico
-routerExchanges.get('/user/:anunciante_id',exchangeController.exchangesByUserId);
+routerExchanges.get('/user/:anunciante_id', authMiddleware, exchangeController.exchangesByUserId);
 
 // POST / --> Cria um novo anuncio
-routerExchanges.post('/', exchangeController.createExchange);
+routerExchanges.post('/', authMiddleware, exchangeController.createExchange);
 
 
 // PUT /:id --> Atualiza os dados de um anúncio específico de acordo com o ID do anúncio
-routerExchanges.put('/:id', exchangeController.updateExchange);
+routerExchanges.put('/:id', authMiddleware, exchangeController.updateExchange);
 
 
 // DELETE /:id --> Deleta os dados de um anúncio específico de acordo com o ID do anúncio;
-routerExchanges.delete('/:id', exchangeController.deleteExchange);
+routerExchanges.delete('/:id', authMiddleware, exchangeController.deleteExchange);
 
 
 // PATH /:id --> Fecha um anúncio (anúncio passa do estado ativo para inativo, 
 //altera "ativo: TRUE" para "ativo: FALSE" e insere o valor da "data_conclusão");
-routerExchanges.patch('/:id',exchangeController.closeExchange);
+routerExchanges.patch('/:id', authMiddleware, exchangeController.closeExchange);
 
 export default routerExchanges;
 
