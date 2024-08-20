b941431541298980254bbba4043b470cf3d2da03
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-20 02:25:14 -0300

    Delete prisma directory

--- prisma/migrations/20240802113524_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[endereco_id]` on the table `usuarios` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "usuarios_endereco_id_key" ON "usuarios"("endereco_id");
